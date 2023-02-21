9632b49a9e289fca2947128a64d07b7ef2338d14
Nmt131199 <manhtrietvt@gmail.com>
2023-02-22 02:06:23 +0700

    finish baseline pipeline

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 import pathlib
+import json
+import numpy as np
 from fastapi import FastAPI
 from typing import Optional
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import tokenizer_from_json
 app = FastAPI()
 
@@ -14,19 +17,59 @@ METADATA_PATH = SMS_SPAM_MODEL_DIR / "spam-classifer-metadata.json"
 
 AI_MODEL = None
 AI_TOKENIZER = None
+MODEL_METADATA = {}
+LEGEND_INVERTED = {}
+labels_legend_inverted = {}
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+    
+def encode_to_json(data, as_py=True):
+    encoded = json.dumps(data, cls=NumpyEncoder)
+    if as_py:
+        return json.loads(encoded)
+    return encoded
+ 
 # This run on strt-up to get our prediction model
 @app.on_event("startup")
 def on_startup():
-    global AI_MODEL, AI_TOKENIZER
+    global AI_MODEL, AI_TOKENIZER, MODEL_METADATA, LEGEND_INVERTED, labels_legend_inverted
     if MODEL_PATH.exists():
         AI_MODEL = load_model(MODEL_PATH)
     if TOKENIZER_PATH.exists():
         t_json = TOKENIZER_PATH.read_text()
         AI_TOKENIZER = tokenizer_from_json(TOKENIZER_PATH.read_text())
-        print(AI_TOKENIZER)
+    if METADATA_PATH.exists():
+        MODEL_METADATA = json.loads(METADATA_PATH.read_text())
+        labels_legend_inverted = MODEL_METADATA['labels_legend_inverted']
+
+def predict(query:str):
+    sequences = AI_TOKENIZER.texts_to_sequences([query])
+    maxlen = MODEL_METADATA.get('max_sequence') or 280
+    x_input = pad_sequences(sequences, maxlen=maxlen)
+   
+    preds_array = AI_MODEL.predict(x_input) # list of preds
+    preds = preds_array[0]
+    top_idx_val = np.argmax(preds)
+    top_pred = {
+        "label": labels_legend_inverted[str(top_idx_val)],
+        "condifence": preds[top_idx_val]}
+    labeled_preds = [{
+        "label": labels_legend_inverted[str(i)],
+        "confidence": x} for i, x in enumerate(list(preds))]
+    return json.loads(json.dumps({"top": top_pred, "predictions": labeled_preds}, cls=NumpyEncoder))
 
 @app.get("/")
 def read_index(q:Optional[str] = None):
-    global AI_MODEL
-    print(AI_MODEL)
-    return {"hello": "world"}
+    global AI_MODEL, MODEL_METADATA, labels_legend_inverted
+    query = q or "hello world"
+    preds_dict = predict(query)
+    return {"query": query, "result": preds_dict}
