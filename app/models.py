63efb62528609452b4d0140e7177ec8460e3023a
Nmt131199 <manhtrietvt@gmail.com>
2023-02-23 01:30:25 +0700

    finish configure astradb + cassandra model

---
+++ app/models.py
@@ -0,0 +1,12 @@
+import uuid
+from cassandra.cqlengine.models import Model
+from cassandra.cqlengine import columns
+
+
+class SMSInference(Model):
+    __keyspace__ = "spam_inferences"
+    uuid = columns.UUID(primary_key=True, default=uuid.uuid1)
+    query = columns.Text()
+    label = columns.Text()  
+    confidence = columns.Float()
+    model_version = columns.Text(default="v1")
